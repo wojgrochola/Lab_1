7f12da8d6bdfacc942f3b48e2b12ac1e2ca063f4
Wojtek <wojgrochola@gmail.com>
2015-11-08 23:18:48 +0100

    Final version

---
+++ daemon.py
@@ -0,0 +1,99 @@
+##daemon nie jest robiony samemu, oparty jest na kodzie z internetu 
+## http://www.jejik.com/articles/2007/02/a_simple_unix_linux_daemon_in_python/
+
+import sys, os, time, atexit
+from signal import SIGTERM
+
+class Daemon:
+
+    def __init__(self, pidfile, stdin='/dev/stdin', stdout='/dev/stdout', stderr='/dev/stderr'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit first parent
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # decouple from parent environment
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        # do second fork
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # exit from second parent
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        # write pidfile
+        pid = str(os.getpid())
+        open(self.pidfile,'w+').write("%s\n" % pid)
+
+    def start(self):
+
+        try:
+            pf = open(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = "pidfile %s already exist. Daemon already running? Use: python exc01.py stop.\n"
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        # Start the daemon
+        self.daemonize()
+        self.run()
+
+    def stop(self):
+        """
+        Stop the daemon
+        """
+        # Get the pid from the pidfile
+        try:
+            pf = open(self.pidfile,'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = "pidfile %s does not exist. Daemon not running?\n"
+            sys.stderr.write(message % self.pidfile)
+            return # not an error in a restart
+
+        # Try killing the daemon process
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError as err:
+            err = str(err)
+            if err.find("No such process") > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print(str(err))
+                sys.exit(1)
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def run(self):
+    	"Your code"
--- exc01.py
+++ exc01.py
@@ -1,56 +1,73 @@
-import commands
-import time
-import sys
-import logging
-class Info:
-    def __init__(self):
-      self.logger = logging.getLogger(__name__)
-      self.logger.setLevel(logging.INFO)
-      
-      self.handler = logging.FileHandler('log.txt')
-      self.handler.setLevel(logging.INFO)
-      
-      formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-      self.handler.setFormatter(formatter)
-      self.logger.addHandler(self.handler)
+#!/usr/bin/env python
+ 
+import sys, time, commands, logging
+from daemon import Daemon
 
-    def getCpu(self):
-      cpu = commands.getoutput("top -n 1 | grep Cpu")
-      return cpu
-    
-    def getRam(self):
-      ram = commands.getoutput('top -n 1 | grep "KiB Mem"')
-      return ram
 
-    def getDuo(self):
-      info = self.getRam() + "\n\t" + self.getCpu();
-      return info
-    
-    def execute(self, flag):
-      print("Program is running with flag: %s. Press CTRL-C or CTRL-Z to stop.\n" % flag)
-      if (flag == '-p'):
-        func = self.getCpu
-      elif (flag == '-r'):
-        func = self.getRam
-      elif (flag == '-pr'):
-        func = self.getDuo
-        
-      while True:
-        self.logger.info(("\n\t" + func()))
-  
-       
-      
-      
+
+
+
+class MyDaemon(Daemon):
+		def __init__(self, filePath):
+			Daemon.__init__(self, filePath)
+			FORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+			logging.basicConfig(filename='log.txt', level=logging.INFO, format=FORMAT)
+		
+		def run(self):
+				while True:
+					self.execute()
+					time.sleep(30)
+
+		def usageInfo(self):
+			print "usage: %s start -flag" % sys.argv[0]
+			print ("Available flags:\n\t-p - log CPU usage\n\t-r \tlog RAM usage\n\t-pr  log CPU and RAM usage.\n")
+			sys.exit(2)
+
+
+		def getCpu(self):
+			cpu = commands.getoutput("top -n 1 | grep Cpu")
+			return cpu
+	
+		def getRam(self):
+			memTotal = float(commands.getoutput('cat /proc/meminfo | grep MemTotal').split()[1])
+			memFree =  float(commands.getoutput('cat /proc/meminfo | grep MemFree').split()[1])
+			memProc = float ((memFree/memTotal) * 100)
+			return str(round(memProc, 2)) + "%"
+
+		def getDuo(self):
+			info = "\n\tRam usage: " + self.getRam() + "\n\t" + self.getCpu();
+			return info
+
+		def execute(self):
+			info = ""
+			if (len(sys.argv) == 3):
+				flag = sys.argv[2]
+				if (flag == '-p'):
+					info = self.getCpu()
+				elif(flag == '-r'):
+					info = self.getRam()
+				elif(flag == '-pr'):
+					info = self.getDuo()
+				else:
+					print ("Unkown flag. Exit program.")
+					sys.exit(2)
+			else:
+				self.usageInfo()
+			logging.info(info)
+			  
+	 
 if __name__ == "__main__":
-  info = Info()
-  if len(sys.argv) == 2:
-    if '-p' == sys.argv[1]:
-      info.execute('-p')
-    elif '-r' == sys.argv[1]:
-      info.execute('-r')                     
-    elif '-pr' == sys.argv[1]:
-      info.execute('-pr')
-    else:
-      print ("Use -p or -r.")
-  else:
-    print ("Invalid use.")
+		daemon = MyDaemon('/tmp/systemLogger.pid')
+		if len(sys.argv) >= 2:
+				if 'start' == sys.argv[1]:
+						daemon.start()
+				elif 'stop' == sys.argv[1]:
+						daemon.stop()
+				elif 'restart' == sys.argv[1]:
+						daemon.restart()
+				else:
+						print "Unknown command. Use start|stop|restart "
+						sys.exit(2)
+				sys.exit(0)
+		else:
+				daemon.usageInfo()
