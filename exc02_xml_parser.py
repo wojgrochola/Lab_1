d1d453ed0a7fb8150fcb2ed33896f3e0c0bab605
Wojtek <wojgrochola@gmail.com>
2015-11-27 00:40:22 +0100

    Xml Parser with Tests

--- exc02_xml_parser.py
+++ exc02_xml_parser.py
@@ -1,35 +1,46 @@
 import xml.etree.ElementTree as ET
-import unittest
+import unittest, sys
+
+
 
 class Parser:
-    def __init__(self):
-        self.tuple_list = []
-        self.tree =  ET.parse('Books.xml')
-    def parseBook(self):
+
+    def __init__(self, name):
+
+        self.name = name
+        self.catalog = []
+
+    def parseXml(self):
+        try:
+            self.tree = ET.parse(self.name)
+        except:
+            print (sys.exc_info()[0])
+            sys.exit(0)
+
+    def run(self):
+        self.parseXml()
         root = self.tree.getroot()
 
+        for child in root:
+            childElements = []
+            for innerChild in child:
+                childElements.append(innerChild.text)
+            self.catalog.append(childElements)
 
-        for book in root.findall('book'):
-            t = (book.get('id'), 
-                book.find('author').text, 
-                book.find('title').text, 
-                book.find('genre').text, 
-                book.find('price').text, 
-                book.find('publish_date').text, 
-                book.find('description').text)
-            self.tuple_list.append(t)
-        return self.tuple_list
-    def printParse(self):
-        print(self.tuple_list[0])
-        for i in self.tuple_list:
-            print('_id: ',i[0])
-            print('    --> author: ', i[1])
-            print('    --> title: ', i[2])
-            print('    --> genre: ', i[3])
+        return self.catalog
 
+    def __repr__(self):
+        out = ''
+        for i in self.catalog:
+            out += '_id: ' + i[0] + '\n'
+            out +='    --> author: ' +  i[1] + '\n'
+            out += '    --> title: ' +  i[2] + '\n'
+            out += '    --> genre: ' +  i[3] + '\n'
 
+        return out
 
 if __name__ == '__main__':
-    p = Parser()
-    ll = p.parseBook()
-    print(ll[0])
+    p = Parser('Books.xml')
+    p.run()
+    print (p)
+
